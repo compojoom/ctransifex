dec127a66a67e0a44c3059f80b6b403489cb1d4e
daycounts <admin@daycounts.com>
2013-04-25 12:05:45 -0300

    Added pathway support (breadcrumb)

--- source/components/com_ctransifex/views/project/view.html.php
+++ source/components/com_ctransifex/views/project/view.html.php
@@ -12,7 +12,10 @@ defined('_JEXEC') or die('Restricted access');
 class ctransifexViewProject extends JViewLegacy {
 
     public function display() {
+        $app = JFactory::getApplication();
+    	$pathway = $app->getPathway();
         $this->item = $this->get('Item');
+        $pathway->addItem($this->item->title);
         $this->languages = $this->get('Languages');
         $document = JFactory::getDocument();
         $document->setTitle($document->getTitle() . ' ' . JText::sprintf('COM_CTRANSIFEX_TRANSLATIONS_FOR_PROJECT',$this->item->title));
