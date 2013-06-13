1c8a2c568650cf4a187729aa06968b027f51ab15
Daniel Dimitrov <daniel@compojoom.com>
2013-06-13 17:56:26 +0200

    changes to the build script (core + pro version)

---
+++ builds/templates/version.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author     Daniel Dimitrov <daniel@compojoom.com>
+ * @date       13.06.13
+ *
+ * @copyright  Copyright (C) 2008 - 2013 compojoom.com . All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+defined('_JEXEC') or die('Restricted Access');
+
+define('CTRANSIFEX_PRO', '@@PRO@@');
+define('CTRANSIFEX_VERSION', '@@VERSION@@');
+define('CTRANSIFEX_DATE', '@@DATE@@');
