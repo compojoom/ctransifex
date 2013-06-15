54872e28fcec3d5bd47f35a0f37a33b9cef19d66
Daniel Dimitrov <daniel@compojoom.com>
2013-06-15 16:44:08 +0200

    pagination was missing from the project view

--- source/components/com_ctransifex/views/projects/tmpl/default.php
+++ source/components/com_ctransifex/views/projects/tmpl/default.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @author Daniel Dimitrov - compojoom.com
- * @date: 21.09.12
+ * @author     Daniel Dimitrov - compojoom.com
+ * @date       : 21.09.12
  *
  * @copyright  Copyright (C) 2008 - 2012 compojoom.com . All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
@@ -15,21 +15,27 @@ JHtml::_('stylesheet', 'media/com_ctransifex/css/ctransifex-frontend.css');
 ?>
 
 <?php foreach ($this->items as $i => $item) : ?>
-    <?php $url = JRoute::_('index.php?option=com_ctransifex&view=project&id=' . $item->id); ?>
-    <section id="<?php echo $item->transifex_slug; ?>">
-        <h2>
-            <a href="<?php echo $url;?>">
-                <?php echo $item->title; ?>
-            </a>
-        </h2>
-        <div>
-            <?php echo $item->description; ?>
-        </div>
-        <a class="btn" href="<?php echo $url; ?>">
-            <?php echo JText::_('COM_CTRANSIFEX_VIEW_AVAILABLE_TRANSLATIONS'); ?>
-        </a>
-        <hr />
-    </section>
+	<?php $url = JRoute::_('index.php?option=com_ctransifex&view=project&id=' . $item->id); ?>
+	<section id="<?php echo $item->transifex_slug; ?>">
+		<h2>
+			<a href="<?php echo $url; ?>">
+				<?php echo $item->title; ?>
+			</a>
+		</h2>
+
+		<div>
+			<?php echo $item->description; ?>
+		</div>
+		<a class="btn" href="<?php echo $url; ?>">
+			<?php echo JText::_('COM_CTRANSIFEX_VIEW_AVAILABLE_TRANSLATIONS'); ?>
+		</a>
+		<hr/>
+	</section>
 <?php endforeach; ?>
+
+<div class="pagination">
+		<p class="counter pull-right"> <?php echo $this->pagination->getPagesCounter(); ?> </p>
+		<?php echo $this->pagination->getPagesLinks(); ?>
+</div>
 
 <?php CTransifexHelperUtils::footer(); ?>
