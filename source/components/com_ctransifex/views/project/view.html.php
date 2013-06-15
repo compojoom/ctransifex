e2a477324c08c1cb93c0b39dbb7fb3234753d52f
Daniel Dimitrov <daniel@compojoom.com>
2013-06-15 10:44:09 +0200

    phpcs & notice

--- source/components/com_ctransifex/views/project/view.html.php
+++ source/components/com_ctransifex/views/project/view.html.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @author Daniel Dimitrov - compojoom.com
- * @date: 21.09.12
+ * @author     Daniel Dimitrov <daniel@compojoom.com>
+ * @date       21.09.12
  *
  * @copyright  Copyright (C) 2008 - 2012 compojoom.com . All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
@@ -9,13 +9,26 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-class ctransifexViewProject extends JViewLegacy {
-
-    public function display() {
-        $this->item = $this->get('Item');
-        $this->languages = $this->get('Languages');
-        $document = JFactory::getDocument();
-        $document->setTitle($document->getTitle() . ' ' . JText::sprintf('COM_CTRANSIFEX_TRANSLATIONS_FOR_PROJECT',$this->item->title));
-        parent::display();
-    }
+/**
+ * Class ctransifexViewProject
+ *
+ * @since  1
+ */
+class CtransifexViewProject extends JViewLegacy
+{
+	/**
+	 * Display.
+	 *
+	 * @param   null  $tpl  - the template
+	 *
+	 * @return mixed|void
+	 */
+	public function display($tpl = null)
+	{
+		$this->item = $this->get('Item');
+		$this->languages = $this->get('Languages');
+		$document = JFactory::getDocument();
+		$document->setTitle($document->getTitle() . ' ' . JText::sprintf('COM_CTRANSIFEX_TRANSLATIONS_FOR_PROJECT', $this->item->title));
+		parent::display();
+	}
 }
