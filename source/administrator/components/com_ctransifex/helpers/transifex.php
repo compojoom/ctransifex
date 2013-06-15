440baf49d98c6cbbf9e781b0bb25f3c3b45e98a5
Daniel Dimitrov <daniel@compojoom.com>
2013-06-15 13:28:47 +0200

    phpcs

--- source/administrator/components/com_ctransifex/helpers/transifex.php
+++ source/administrator/components/com_ctransifex/helpers/transifex.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @author Daniel Dimitrov - compojoom.com
- * @date: 24.09.12
+ * @author     Daniel Dimitrov <daniel@compojoom.com>
+ * @date       24.09.12
  *
  * @copyright  Copyright (C) 2008 - 2012 compojoom.com . All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
@@ -9,81 +9,107 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-class ctransifexHelperTransifex
+/**
+ * Class ctransifexHelperTransifex
+ *
+ * @since  1
+ */
+class CtransifexHelperTransifex
 {
-    private static $apiUrl = 'https://www.transifex.com/api/2/project/';
-    private static $languages = array();
-    /**
-     * @param $path
-     * @return array
-     */
-    public static function getData($path)
-    {
-        $config = JComponentHelper::getParams('com_ctransifex');
-        $url = self::$apiUrl . $path;
-        $ch = curl_init();
-        $info = '';
-        $timeout = 120;
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_USERPWD, $config->get('tx_username').":".$config->get('tx_password'));
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 400);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        // get the data
-        $data = curl_exec($ch);
-        // get info about the request
-        $info = curl_getinfo($ch);
+	private static $apiUrl = 'https://www.transifex.com/api/2/project/';
 
-        // close the request
-        curl_close($ch);
+	private static $languages = array();
 
-        return array('data' => $data, 'info' => $info);
-    }
+	/**
+	 * Gets data from transifex
+	 *
+	 * @param   string  $path  - the url
+	 *
+	 * @return array
+	 */
+	public static function getData($path)
+	{
+		$config = JComponentHelper::getParams('com_ctransifex');
+		$url = self::$apiUrl . $path;
+		$ch = curl_init();
+		$info = '';
+		$timeout = 120;
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_USERPWD, $config->get('tx_username') . ":" . $config->get('tx_password'));
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 400);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-    /**
-     * Gets the transifex langCode and transforms it to the joomla language code
-     * @param $transifexLang
-     * @param $projectConfig
-     * @internal param $config
-     * @return mixed
-     */
-    public static function getJLangCode($transifexLang, $projectConfig){
+		// Get the data
+		$data = curl_exec($ch);
 
+		// Get info about the request
+		$info = curl_getinfo($ch);
 
-        $languages = self::getLangmap($projectConfig);
+		// Close the request
+		curl_close($ch);
 
-        if(isset($languages[$transifexLang])) {
-            return $languages[$transifexLang];
-        } else {
-            JLog::addLogger(array('text_file' => 'com_ctransifex.error.php'));
-            JLog::add('there is no lang map entry for ' . $transifexLang);
+		return array('data' => $data, 'info' => $info);
+	}
 
-            return false;
-        }
+	/**
+	 * Gets the transifex langCode and transforms it to the joomla language code
+	 *
+	 * @param   string  $transifexLang  - the lang
+	 * @param   object  $projectConfig  - the config
+	 *
+	 * @internal param $config
+	 * @return mixed
+	 */
+	public static function getJLangCode($transifexLang, $projectConfig)
+	{
+		$languages = self::getLangmap($projectConfig);
 
-        return $langs[$transifexLang];
-    }
+		if (isset($languages[$transifexLang]))
+		{
+			return $languages[$transifexLang];
+		}
+		else
+		{
+			JLog::addLogger(array('text_file' => 'com_ctransifex.error.php'));
+			JLog::add('there is no lang map entry for ' . $transifexLang);
+		}
 
-    private function getLangmap($projectConfig) {
+		return false;
+	}
 
-        if(!count(self::$languages)) {
-            $componentConfig = JComponentHelper::getParams('com_ctransifex');
-            $langMap = explode(',', $componentConfig->get('tx_lang_map'));
-            foreach($langMap as $map) {
-                $langCodes = explode(':', $map);
-                $languages[trim($langCodes[0])] = trim($langCodes[1]);
-            }
+	/**
+	 * Gets the language map
+	 *
+	 * @param   object  $projectConfig  - the project config
+	 *
+	 * @return array
+	 */
+	private static function getLangmap($projectConfig)
+	{
+		if (!count(self::$languages))
+		{
+			$componentConfig = JComponentHelper::getParams('com_ctransifex');
+			$langMap = explode(',', $componentConfig->get('tx_lang_map'));
 
-            $langMap = explode(',', $projectConfig['main']['lang_map']);
-            foreach($langMap as $map) {
-                $langCodes = explode(':', $map);
-                $languages[trim($langCodes[0])] = trim($langCodes[1]);
-            }
+			foreach ($langMap as $map)
+			{
+				$langCodes = explode(':', $map);
+				$languages[trim($langCodes[0])] = trim($langCodes[1]);
+			}
 
-            self::$languages = $languages;
-        }
+			$langMap = explode(',', $projectConfig['main']['lang_map']);
 
-        return self::$languages;
-    }
+			foreach ($langMap as $map)
+			{
+				$langCodes = explode(':', $map);
+				$languages[trim($langCodes[0])] = trim($langCodes[1]);
+			}
+
+			self::$languages = $languages;
+		}
+
+		return self::$languages;
+	}
 }
