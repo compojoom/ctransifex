cd3cc2f8c98304416e3f38b63b901a8c31fce9d4
Daniel Dimitrov <daniel@compojoom.com>
2013-06-15 13:30:17 +0200

    phpcs & notice

--- source/administrator/components/com_ctransifex/views/projects/view.html.php
+++ source/administrator/components/com_ctransifex/views/projects/view.html.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @author Daniel Dimitrov - compojoom.com
- * @date: 21.09.12
+ * @author     Daniel Dimitrov <daniel@compojoom.com>
+ * @date       21.09.12
  *
  * @copyright  Copyright (C) 2008 - 2012 compojoom.com . All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
@@ -9,22 +9,41 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-class ctransifexViewProjects extends JViewLegacy {
+/**
+ * Class ctransifexViewProjects
+ *
+ * @since  1
+ */
+class ctransifexViewProjects extends JViewLegacy
+{
+	/**
+	 * Display
+	 *
+	 * @param   string  $tpl  the layout
+	 *
+	 * @return mixed|void
+	 */
+	public function display($tpl = null)
+	{
+		$this->items = $this->get('Items');
+		$this->pagination = $this->get('Pagination');
+		$this->state = $this->get('State');
 
-    public function display() {
-        $this->items		= $this->get('Items');
-        $this->pagination	= $this->get('Pagination');
-        $this->state		= $this->get('State');
+		$this->addToolbar();
+		parent::display();
+	}
 
-        $this->addToolbar();
-        parent::display();
-    }
-
-    private function addToolbar(){
-        JToolBarHelper::title(JText::_('COM_CTRANSIFEX_PROJECTS'), 'projects');
-        JToolBarHelper::addNew('project.add');
-        JToolBarHelper::editList('project.edit');
-        JToolBarHelper::deleteList('project.delete');
-        JToolbarHelper::preferences('com_ctransifex');
-    }
+	/**
+	 * Ads toolbar to the backend
+	 *
+	 * @return void
+	 */
+	private function addToolbar()
+	{
+		JToolBarHelper::title(JText::_('COM_CTRANSIFEX_PROJECTS'), 'projects');
+		JToolBarHelper::addNew('project.add');
+		JToolBarHelper::editList('project.edit');
+		JToolBarHelper::deleteList('project.delete');
+		JToolbarHelper::preferences('com_ctransifex');
+	}
 }
