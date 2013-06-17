83b69b3bb4226c08844dd5ce31291354f7fc4355
Daniel Dimitrov <daniel@compojoom.com>
2013-06-17 16:15:19 +0200

    phpcs

--- source/administrator/components/com_ctransifex/models/fields/webhook.php
+++ source/administrator/components/com_ctransifex/models/fields/webhook.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @author Daniel Dimitrov - compojoom.com
- * @date: 22.07.12
+ * @author     Daniel Dimitrov <daniel@compojoom.com>
+ * @date       22.07.12
  *
  * @copyright  Copyright (C) 2008 - 2012 compojoom.com . All rights reserved.
  * @license    GNU General Public License version 2 or later; see LICENSE
@@ -9,42 +9,49 @@
 
 defined('_JEXEC') or die('Restricted access');
 
-class JFormFieldWebhook extends JFormFieldText {
+/**
+ * Class JFormFieldWebhook
+ *
+ * @since  1
+ */
+class JFormFieldWebhook extends JFormFieldText
+{
 
-    /**
-     * The form field type.
-     *
-     * @var    string
-     *
-     * @since  11.1
-     */
-    protected $type = 'Webhook';
+	/**
+	 * The form field type.
+	 *
+	 * @var    string
+	 *
+	 * @since  11.1
+	 */
+	protected $type = 'Webhook';
 
-    /**
-     * Method to get the field input markup.
-     *
-     * @return  string  The field input markup.
-     *
-     * @since   11.1
-     */
-    protected function getInput()
-    {
-        // Initialize some field attributes.
-        $size = $this->element['size'] ? ' size="' . (int) $this->element['size'] . '"' : '';
-        $maxLength = $this->element['maxlength'] ? ' maxlength="' . (int) $this->element['maxlength'] . '"' : '';
-        $class = $this->element['class'] ? ' class="' . (string) $this->element['class'] . '"' : '';
-        $readonly = ((string) $this->element['readonly'] == 'true') ? ' readonly="readonly"' : '';
-        $disabled = ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
+	/**
+	 * Method to get the field input markup.
+	 *
+	 * @return  string  The field input markup.
+	 *
+	 * @since   11.1
+	 */
+	protected function getInput()
+	{
+		// Initialize some field attributes.
+		$size = $this->element['size'] ? ' size="' . (int) $this->element['size'] . '"' : '';
+		$maxLength = $this->element['maxlength'] ? ' maxlength="' . (int) $this->element['maxlength'] . '"' : '';
+		$class = $this->element['class'] ? ' class="' . (string) $this->element['class'] . '"' : '';
+		$readonly = ((string) $this->element['readonly'] == 'true') ? ' readonly="readonly"' : '';
+		$disabled = ((string) $this->element['disabled'] == 'true') ? ' disabled="disabled"' : '';
 
-        // Initialize JavaScript field attributes.
-        $onchange = $this->element['onchange'] ? ' onchange="' . (string) $this->element['onchange'] . '"' : '';
+		// Initialize JavaScript field attributes.
+		$onchange = $this->element['onchange'] ? ' onchange="' . (string) $this->element['onchange'] . '"' : '';
 
 
-        if(!$this->value) {
-            $this->value = md5(JFactory::getUser()->id . JFactory::getUser()->name . microtime());
-        }
+		if (!$this->value)
+		{
+			$this->value = md5(JFactory::getUser()->id . JFactory::getUser()->name . microtime());
+		}
 
-        return '<input type="hidden" name="' . $this->name . '" id="' . $this->id . '"' . ' value="'
-            . htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"' . $class . $size . $disabled . $readonly . $onchange . $maxLength . '/>';
-    }
+		return '<input type="hidden" name="' . $this->name . '" id="' . $this->id . '"' . ' value="'
+		. htmlspecialchars($this->value, ENT_COMPAT, 'UTF-8') . '"' . $class . $size . $disabled . $readonly . $onchange . $maxLength . '/>';
+	}
 }
